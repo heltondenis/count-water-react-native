97eab5380bb99f395d0356d27ecd44e7fd1d212a
Helton Denis <heltondeniss@gmail.com>
2018-06-16 18:30:48 -0300

    Desenvolvimento geral do App

--- App.js
+++ App.js
@@ -1,58 +1,124 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 import React, { Component } from 'react';
-import {
-  Platform,
-  StyleSheet,
-  Text,
-  View
-} from 'react-native';
+import { Platform, StyleSheet, Text, View, ImageBackground, Button, Alert } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
 
-type Props = {};
-export default class App extends Component<Props> {
+export default class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {consumido:0, status:'Ruim', pct:0};
+
+    this.addCopo = this.addCopo.bind(this);
+    this.atualizar = this.atualizar.bind(this);
+  }
+
+  atualizar(){
+    let s = this.state;
+    s.pct = Math.floor(((s.consumido/2000)*100));
+
+    if (s.pct >= 100) {
+      s.status = "Bom";
+    } else {
+      s.status = "Ruim";
+    }
+
+    if (s.consumido == 2000) {
+      Alert.alert('Boa! Você bateu sua meta diária!');
+    }
+    if (s.consumido >= 2600 && s.consumido <= 3600) {
+      Alert.alert('Cuidado! Agua demais também não faz bem!')
+      s.status = "Ruim";
+    }
+    if (s.consumido >= 3600) {
+      Alert.alert('Para com isso! Você vai passar mal!')
+      s.status = "Passando Mal!";
+    }
+
+    this.setState(s);
+  }
+
+  addCopo(){
+    let s = this.state;
+    s.consumido += 200;
+    this.setState(s);
+
+    this.atualizar();
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+      <View style={styles.body}>
+        <ImageBackground source={require('./images/waterbg.png')} style={styles.bgimage}>
+        <View>
+
+          <View style={styles.infoArea}>
+            <View style={styles.area}>
+              <Text style={styles.areaTitulo}>Meta</Text>
+            <Text style={styles.areaDado}>200ml</Text>
+            </View>
+            <View style={styles.area}>
+              <Text style={styles.areaTitulo}>Consumido</Text>
+            <Text style={styles.areaDado}>{this.state.consumido}</Text>
+            </View>
+            <View style={styles.area}>
+              <Text style={styles.areaTitulo}>Status</Text>
+            <Text style={styles.areaDado}>{this.state.status}</Text>
+            </View>
+          </View>
+
+          <View style={styles.pctArea}>
+            <Text style={styles.pctText}>{this.state.pct}%</Text>
+          </View>
+
+          <View style={styles.btnArea}>
+            <Button title="Beber 200ml" onPress={this.addCopo} />
+          </View>
+
+        </View>
+        </ImageBackground>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    body:{
+      flex:1,
+      paddingTop:20
+    },
+    bgimage:{
+      flex:1,
+      width:null
+    },
+    infoArea:{
+      flex:1,
+      flexDirection:'row',
+      marginTop:70
+    },
+    area:{
+      flex:1,
+      alignItems:'center'
+    },
+    areaTitulo:{
+      color:'#45b2fc',
+      fontSize: 18
+    },
+    areaDado:{
+      color:'#2b4274',
+      fontSize:15,
+      fontWeight:'bold'
+    },
+    pctArea:{
+      marginTop:180,
+      alignItems:'center',
+    },
+    pctText:{
+      fontSize:70,
+      color:'#FFFFFF',
+      backgroundColor:'transparent'
+    },
+    btnArea:{
+      marginTop: 30,
+      alignItems:'center'
+    }
 });
